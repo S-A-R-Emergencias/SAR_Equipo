f380a35b2a3bf0f9c200c6794c91973053f4f84d
FernandoTFW <fer.aparicio1811@gmail.com>
2022-09-04 01:07:44 -0400

    Se ha acabado la página del perfil de personal del sar

--- sar_equipo/lib/main.dart
+++ sar_equipo/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sar_equipo/src/main_web_page.dart';
+import 'package:sar_equipo/src/profile/profile_page.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 void main (){
