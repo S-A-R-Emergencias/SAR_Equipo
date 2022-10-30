205e307fea875f7a2104106b2e4abe8a8d88603b
MarceloApala <cam5000810@est.univalle.edu>
2022-10-29 20:17:46 -0400

    Providers sin UI

--- sar_equipo/lib/src/pages/element_web_page.dart
+++ sar_equipo/lib/src/pages/element_web_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:provider/provider.dart';
 
 import '../../Models/element_model.dart';
