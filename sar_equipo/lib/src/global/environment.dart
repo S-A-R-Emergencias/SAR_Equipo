19843e886237276d690e42d5991f4740ba8f8fc6
FernandoTFW <fer.aparicio1811@gmail.com>
2022-11-15 22:47:50 -0400

    vinculacion para despliegue

--- sar_equipo/lib/src/global/environment.dart
+++ sar_equipo/lib/src/global/environment.dart
@@ -1,7 +1,7 @@
 import '../../Models/personnel_model.dart';
 
 class Environment {
-  static String apiURL = "http://localhost:3000/api";
+  static String apiURL = "https://restapinodejssar-production.up.railway.app/api";
   static Personnel? usersession = null;
   static String getImageUrl(int id,String type){
     return "https://res.cloudinary.com/dza50jbso/image/upload/c_scale,w_500/"+id.toString()+"_"+type;
